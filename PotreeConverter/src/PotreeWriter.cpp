a15d8544998c8838ddd45bcb5a2dfba316a97d71
Boxian Dong <bdong@esri.com>
2024-07-03 15:59:03 +0200

    solving c++17 compiling problem

--- PotreeConverter/src/PotreeWriter.cpp
+++ PotreeConverter/src/PotreeWriter.cpp
@@ -295,7 +295,8 @@ void PWNode::flush(){
 			if(fs::exists(temppath)){
 				PointReader *reader = createReader(temppath);
 				while(reader->readNextPoint()){
-					writer->write(reader->getPoint());
+					auto point = reader->getPoint();
+					writer->write(point);
 				}
 				reader->close();
 				delete reader;
